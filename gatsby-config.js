27e1795ab92bfb9bb953b9e542eb9bd9d1aabc28
Joaquim <juakacc@gmail.com>
2020-06-03 23:36:51 -0300

    Configuring PWA

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,19 @@
 module.exports = {
-  plugins: ["gatsby-theme-material-ui", `gatsby-plugin-react-helmet`],
+  plugins: [
+    "gatsby-theme-material-ui",
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Olivedos_covid-19`,
+        short_name: `OliCovid19`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+      },
+    },
+    `gatsby-plugin-offline`,
+  ],
   pathPrefix: `/covid19`,
 }
